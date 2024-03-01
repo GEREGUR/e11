4c464d575324b549eebe1f123513cfbfc28fe629
George <georgerubaylo@gmail.com>
2024-03-01 16:20:11 +0300

    updated visuals

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Test = () => {
+
+  useEffect(() => {
+    console.log('rendered')
+  },[])
+
   return (
     <div>
-        testpage
+        
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,6 +31,11 @@ const config: Config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
       },
     },
   },
